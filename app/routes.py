6d6825d2cb4dc1e1f09d98234154478e2b276f42
Vijay Nagulan <blazer-sonst0k@icloud.com>
2023-05-06 23:21:32 +0200

    add bootstrap - base.html

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
-from flask import render_template, request
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return "<h1>Hello, Flask!!</h1>"
+    return render_template('home.html')
