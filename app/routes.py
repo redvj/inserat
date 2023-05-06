d0aef390cdc228b38f312c672bfc83bdab6158d5
Vijay Nagulan <blazer-sonst0k@icloud.com>
2023-05-07 00:58:48 +0200

    add 404.html

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,15 @@
 from flask import render_template
 from app import app
 
+# 
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html')
+
+# Create Custem Error pages 
+# Invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+    # Render the template with the 404 error status code
+    return render_template('404.html'), 404
