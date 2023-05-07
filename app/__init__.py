c50946fe9adcc99240849d8ea4df6cbfa4834a5a
Vijay Nagulan <blazer-sonst0k@icloud.com>
2023-05-07 02:11:32 +0200

    add login-form

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
 
+# Load the config.py file
+app.config.from_object(Config)
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
 
 from app import routes
