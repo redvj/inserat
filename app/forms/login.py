3b153c03a139b69b079df093a8c0805627df822f
Vijay Nagulan <blazer-sonst0k@icloud.com>
2023-05-13 15:27:03 +0200

    add login

--- app/forms/login.py
+++ app/forms/login.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms.validators import DataRequired, Length, Email, ValidationError, EqualTo
+from app.models.login import User
 
 
 class LoginForm(FlaskForm):
