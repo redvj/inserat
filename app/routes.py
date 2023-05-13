345a73c0f5ef20e1efa1eb0dbf57c1f8557982ba
Vijay Nagulan <blazer-sonst0k@icloud.com>
2023-05-14 00:20:04 +0200

    added register

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,18 @@
 from flask import render_template, url_for, flash, redirect, request
 from app import app
+
 from app.forms.login import LoginForm
 from app.forms.job import JobForm
-from werkzeug.urls import url_parse
+
 from app.models.login import User
+from app.models.registration import RegistrationForm
+
+from werkzeug.urls import url_parse
 from flask_login import logout_user
 from flask_login import login_required
 
 from flask_login import login_user, current_user
+
 
 #---------------------------------------------------------------------------------
 #------------- Startpage -----------------------------------------
@@ -98,6 +103,29 @@ def create_job():
         flash('Job created successfully!', 'success')
         return redirect(url_for('index'))
     return render_template('create_job.html', form=form)
+
+
+#---------------------------------------------------------------------------------
+
+#---------------------------------------------------------------------------------
+#------------- Registration Form --------------------------------------------------------
+#---------------------------------------------------------------------------------
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
 
 
 #---------------------------------------------------------------------------------
