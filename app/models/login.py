bc2cc28bb8319b5022b6b3a56defff4d7c925f7c
Vijay Nagulan <blazer-sonst0k@icloud.com>
2023-05-21 03:15:48 +0200

    adedd last_seen

--- app/models/login.py
+++ app/models/login.py
@@ -13,7 +13,7 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
     user_adedd = db.Column(db.DateTime, default=datetime.utcnow)
-   
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, url_for, flash, redirect, request, flash
 from app import app, db
-
+from datetime import datetime
 from werkzeug.urls import url_parse
 from flask_login import logout_user
 from flask_login import login_required
@@ -110,6 +110,11 @@ def user(username):
 
 
 #---------------------------------------------------------------------------------
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 #---------------------------------------------------------------------------------
 #------------- Logout --------------------------------------------------------
