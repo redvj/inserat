fa2a6ef1ee95fc279c0069693766eebb6b850c20
Vijay Nagulan <blazer-sonst0k@icloud.com>
2023-08-10 19:38:17 +0200

    update

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ from app.models.login import User
 # Check if the script is being run directly
 if __name__ == '__main__':
     # Start the Flask development server with debugging enabled
-    #app.run(debug=False)
+   
     app.run()
